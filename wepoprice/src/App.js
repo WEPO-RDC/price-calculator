3f9583100b624b4b6bf2ad835121adea6eecb0fd
LeGrandMAG <magmukendi@hufs.ac.kr>
2022-07-07 18:01:19 +0900

    update the title

--- wepoprice/src/App.js
+++ wepoprice/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="appIcon.png" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +20,65 @@ function App() {
         >
           Learn React
         </a>
+        <img src="./appIcon.png"></img>
       </header>
     </div>
   );
 }
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: '',
+      dhl: '',
+      fedex: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: (event.target.value)
+    });
+  }
+  handleSubmit(event) {
+    // Change code below this line
+    let sum = (((this.state.input) *0.00076 + 15+5)/.7)
+    let totSum = Number.parseInt(sum + (sum *10/100))
+this.setState({
+  submit: totSum,
+  dhl: totSum + 14 +"$",
+  fedex: totSum + 3 +"$"
+  })
+  event.preventDefault()
 
-export default App;
+  }
+  render() {
+    return (
+      <body className='Main'>
+        <h1>WEPO Price Calculator</h1>
+        <div className='calculator'>
+          <form onSubmit={this.handleSubmit} className="form">
+            <input value={this.state.input} onChange={this.handleChange} placeholder="Input the buying price here"/>
+            <button type='submit'>Submit!</button>
+          </form>
+            <div className='result-container'>
+              <div className='result'>
+                <h1>DHL</h1>
+                <h1>{this.state.dhl}</h1>
+              </div>
+              
+              <div className='result'>
+                <h1>Fedex</h1>
+                <h1>{this.state.fedex}</h1>
+              </div>
+            </div>
+        </div>
+      </body>
+    );
+  }
+}
+
+
+export default MyForm;
